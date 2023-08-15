a6fe3089a6825351886c5b004b9b09b86e8aa479
Cycododge <cycododge@gmail.com>
2023-08-15 09:07:14 -0700

    add a demo config with defaults

---
+++ eyas.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	appInput: `dist`,
+	serverPort: 443,
+	appUrl: ``,
+	buildVersion: (process.env.CURRENT_BRANCH || `Unknown Source Branch`).trim(),
+	appTitle: ``,
+	appWidth: 1024,
+	appHeight: 768,
+	menu: []
+};
