3fb0f528d3daf599e4234722432e2df49b1134da
Eric Higginson <eric.higginson@cycosoft.com>
2023-08-16 09:53:23 -0700

    grab the branch name for the app version

--- eyas.config.js
+++ eyas.config.js
@@ -1,8 +1,13 @@
+//get the current branch name for the version number
+const execSync = require(`child_process`).execSync;
+const branch = execSync(`git rev-parse --abbrev-ref HEAD`).toString().trim();
+
+//export the config for the project
 module.exports = {
 	testSourceDirectory: `demo`,
 	serverPort: 3000,
 	appUrl: null,
-	buildVersion: process.env.CURRENT_BRANCH,
+	buildVersion: `${branch}`,
 	appTitle: `Demo App`,
 	appWidth: 1024,
 	appHeight: 768,
