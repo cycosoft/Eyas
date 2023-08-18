4168cd381ae05b8edf5b64733faf6a265a7e4212
Eric Higginson <eric.higginson@cycosoft.com>
2023-08-18 08:57:11 -0700

    experimental changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 // imports
-const { app: electronLayer, BrowserWindow, Menu, dialog, shell } = require('electron');
+const electronPath = require.resolve('electron');
+console.log({electronPath});
+const { app: electronLayer, BrowserWindow, Menu, dialog, shell } = require(electronPath);
+console.log({electronLayer});
 const express = require('express');
 const path = require('path');
 const serverLayer = require('https');
