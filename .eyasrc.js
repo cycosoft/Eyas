1ac76153fb875af4b33e8f82812b019d9e536dad
Eric Higginson <eric.higginson@cycosoft.com>
2023-08-25 18:06:50 -0700

    add some test domain variants

--- .eyasrc.js
+++ .eyasrc.js
@@ -1,12 +1,17 @@
 //get the current branch name for the version number
-const execSync = require(`child_process`).execSync;
+const { execSync } = require(`child_process`);
 const branch = execSync(`git rev-parse --abbrev-ref HEAD`).toString().trim();
 
 //export the config for the project
 module.exports = {
 	testSourceDirectory: `src/demo`,
 	// serverPort: 3000,
+	// customDomain: `142.250.217.142`,
+	// customDomain: `142.250.217.142:80`,
+	// customDomain: `test.google.com`,
+	// customDomain: `test.google.com:80`,
 	customDomain: `https://test.google.com`,
+	// customDomain: `https://test.google.com:80`,
 	buildVersion: `${branch}`,
 	appTitle: `Demo App`,
 	appWidth: 1024,
