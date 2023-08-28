acd6556119bdf78e29be63aa9b2f8e6650280bf0
Eric Higginson <eric.higginson@cycosoft.com>
2023-08-27 18:39:05 -0700

    add a script to test the bundler script

--- src/bundler.js
+++ src/bundler.js
@@ -4,16 +4,19 @@
 
 'use strict';
 
-console.log(`Hello World!`);
+// async iife
+(async () => {
+	console.log(`Hello World!`);
 
 
-// At the end of your application code, add the following line:
-const readline = require(`readline`);
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
+	// At the end of your application code, add the following line:
+	const readline = require(`readline`);
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
 
-rl.question(`Press Enter to exit...`, () => {
-	rl.close();
-});
+	rl.question(`Press Enter to exit...`, () => {
+		rl.close();
+	});
+})();
