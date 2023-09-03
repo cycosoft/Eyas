8e4a4a4b4ee0fe4f07f2b3f0e6252280a36cf225
Eric Higginson <eric.higginson@cycosoft.com>
2023-09-02 17:09:37 -0700

    folder structure update

--- src/cli/compile.js
+++ src/cli/compile.js
@@ -7,6 +7,10 @@
 // Import dependencies
 const bytenode = require(`bytenode`);
 const path = require(`path`);
+const fs = require(`fs-extra`);
+
+// await fs.ensureDir(dist);
+// fs.copy(packageFrom, packageTo);
 
 // Compile the CLI with options
 bytenode.compileFile({
@@ -15,3 +19,10 @@ bytenode.compileFile({
 	filename: path.join(process.cwd(), `src`, `cli`, `index.js`),
 	output: path.join(process.cwd(), `dist`, `cli.jsc`)
 });
+
+
+// Copy related files to dist
+const packageJson = `package.json`;
+const packageFrom = path.join(process.cwd(), `src`, `resources`, packageJson);
+const dist = path.join(process.cwd(), `dist`);
+const packageTo = path.join(dist, packageJson);
