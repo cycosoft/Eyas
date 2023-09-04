2406d0d20789d4e265d563d67b89bed0a0ed9c04
Eric Higginson <eric.higginson@cycosoft.com>
2023-09-04 16:46:45 -0700

    Update .eyasrc.js to be in a new format

--- .eyasrc.js
+++ .eyasrc.js
@@ -3,14 +3,33 @@ module.exports = {
 	test: {
 		source: `demo`, // dist
 		port: ``, // 3000
-		domain: ``, // `locahost:${port}`
-		title: ``, // `Eyas`
+		domain: `eyas.cycosoft.com`, // `locahost:${port}`
+		title: `Eyas App Demo`, // `Eyas`
 		version: ``, // current branch name
 		resolutions: [ // prepends to menu of resolutions; defaults to first in list
 			{ label: `SD Desktop`, width: 1024, height: 768 },
 		],
 		menu: [ // Adds a menu items of custom links
-			{ label: `Cycosoft.com (browser)`, url: `https://cycosoft.com`, external: true }
+			{ label: `Cycosoft.com (browser)`, url: `https://cycosoft.com`, external: true },
+			{ url: `server` },
+			{ label: `Cycosoft.com (electron)`, url: `https://cycosoft.com` },
+			{ label: `Cycosoft.com (browser)`, url: `https://cycosoft.com`, external: true },
+			{ url: `https://142.250.217.142` },
+			{ url: `142.250.217.142` },
+			{ url: `142.250.217.142/` },
+			{ url: `142.250.217.142:80` },
+			{ url: `142.250.217.142:80/` },
+			{ url: `www.test.google.com` },
+			{ url: `google.com` },
+			{ url: `test.google.com` },
+			{ url: `demo.google.com` },
+			{ url: `test.google.com/` },
+			{ url: `test.google.com:80` },
+			{ url: `https://test.google.com` },
+			{ url: `https://test.google.com:80` },
+			{ url: `https://www.google.com` },
+			{ url: `https://www.google.com/` },
+			{ url: `https://localhost:3000`, external: false }
 		]
 	},
 
@@ -20,34 +39,5 @@ module.exports = {
 		windows: undefined, // undefined
 		mac: undefined, // undefined
 		linux: undefined // undefined
-	},
-
-	testSourceDirectory: `demo`,
-	// serverPort: 3000,
-	customDomain: `eyas.cycosoft.com`,
-	// buildVersion: `v${version}`,
-	appTitle: `Eyas App Demo`,
-	appWidth: 1024,
-	appHeight: 768,
-	menu: [
-		{ url: `server` },
-		{ label: `Cycosoft.com (electron)`, url: `https://cycosoft.com` },
-		{ label: `Cycosoft.com (browser)`, url: `https://cycosoft.com`, external: true },
-		{ url: `https://142.250.217.142` },
-		{ url: `142.250.217.142` },
-		{ url: `142.250.217.142/` },
-		{ url: `142.250.217.142:80` },
-		{ url: `142.250.217.142:80/` },
-		{ url: `www.test.google.com` },
-		{ url: `google.com` },
-		{ url: `test.google.com` },
-		{ url: `demo.google.com` },
-		{ url: `test.google.com/` },
-		{ url: `test.google.com:80` },
-		{ url: `https://test.google.com` },
-		{ url: `https://test.google.com:80` },
-		{ url: `https://www.google.com` },
-		{ url: `https://www.google.com/` },
-		{ url: `https://localhost:3000`, external: false }
-	]
+	}
 };
