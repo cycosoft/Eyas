30e4947d443e8b5625873c0b619c882a1bc6fa90
Eric Higginson <eric.higginson@cycosoft.com>
2023-09-05 06:44:57 -0700

    adjust the path from src/main/ to src/eyas/

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -1,9 +1,21 @@
+/* global __dirname */
+
+// imports
+import { resolve } from 'path';
 import { bytecodePlugin } from 'electron-vite';
 
+// export the configuration settings
 export default {
 	main: {
 		plugins: [bytecodePlugin()],
 		build: {
+			rollupOptions: {
+				input: {
+					// update the path to the entry point
+					index: resolve(__dirname, `src/eyas/index.js`)
+				}
+			},
+
 			outDir: `dist/eyas`
 		}
 	}
