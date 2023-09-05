d057145e8b3e4b609a334d4e7c7f4797455aa0d7
Eric Higginson <eric.higginson@cycosoft.com>
2023-09-04 18:03:32 -0700

    move file and folder names to constants

--- src/scripts/paths.js
+++ src/scripts/paths.js
@@ -12,38 +12,64 @@ const moduleRoot = isProd
 	? path.join(consumerRoot, `node_modules`, `@cycosoft`, `eyas`)
 	: consumerRoot;
 
+// file and folder names
+const names = {
+	files: {
+		eyasrcJs: `.eyasrc.js`,
+		cliJs: `cli.js`,
+		cliJsc: `cli.jsc`,
+		getConfigJs: `get-config.js`,
+		packageJson: `package.json`
+	},
+
+	folders: {
+		buildAssets: `build-assets`,
+		eyasAssets: `eyas-assets`,
+		cli: `cli`,
+		eyas: `eyas`,
+		scripts: `scripts`,
+		test: `test`
+	}
+};
+
+// base paths
+const roots = {
+	dist: path.join(moduleRoot, `dist`),
+	src: path.join(moduleRoot, `src`),
+	eyasBuild: path.join(consumerRoot, `.eyas-build`),
+	eyasDist: path.join(consumerRoot, `.eyas-dist`)
+};
+
 // object to be exported
 const paths = {
 	module: {
-		dist: path.join(moduleRoot, `dist`),
-		buildAssetsSrc: path.join(moduleRoot, `src`, `build-assets`),
-		buildAssetsDest: path.join(moduleRoot, `dist`, `build-assets`),
-		eyasAssetsSrc: path.join(moduleRoot, `src`, `eyas-assets`),
-		eyasAssetsDest: path.join(moduleRoot, `dist`, `eyas-assets`),
-		cliDest: path.join(moduleRoot, `dist`, `cli`),
-		cliSrcFile: path.join(moduleRoot, `src`, `cli`, `index.js`),
-		cliDestFile: path.join(moduleRoot, `dist`, `cli`, `index.jsc`)
+		dist: roots.dist,
+		buildAssetsSrc: path.join(roots.src, names.folders.buildAssets),
+		buildAssetsDest: path.join(roots.dist, names.folders.buildAssets),
+		eyasAssetsSrc: path.join(roots.src, names.folders.eyasAssets),
+		eyasAssetsDest: path.join(roots.dist, names.folders.eyasAssets),
+		cliDest: path.join(roots.dist, names.folders.cli),
+		cliSrcFile: path.join(roots.src, names.folders.cli, names.files.cliJs),
+		cliDestFile: path.join(roots.dist, names.folders.cli, names.files.cliJsc)
 	},
 
 	cli: {
-		build: path.join(consumerRoot, `.eyas-build`),
-		dist: path.join(consumerRoot, `.eyas-dist`),
-		eyasSrc: path.join(moduleRoot, `dist`, `eyas`),
-		eyasDest: path.join(consumerRoot, `.eyas-build`),
-		getConfigScript: path.join(moduleRoot, `src`, `scripts`, `get-config.js`),
-		configDest: path.join(consumerRoot, `.eyas-build`, `.eyasrc.js`),
-		testDest: path.join(consumerRoot, `.eyas-build`, `test`),
-		eyasAssetsSrc: path.join(moduleRoot, `dist`, `eyas-assets`),
-		eyasAssetsDest: path.join(consumerRoot, `.eyas-build`, `eyas-assets`),
-		packageJsonSrc: path.join(moduleRoot, `dist`, `build-assets`, `package.json`),
-		packageJsonDest: path.join(consumerRoot, `.eyas-build`, `package.json`)
+		build: roots.eyasBuild,
+		dist: roots.eyasDist,
+		eyasSrc: path.join(roots.dist, names.folders.eyas),
+		eyasDest: roots.eyasBuild,
+		getConfigScript: path.join(roots.src, names.folders.scripts, names.files.getConfigJs),
+		configDest: path.join(roots.eyasBuild, names.files.eyasrcJs),
+		testDest: path.join(roots.eyasBuild, names.folders.test),
+		eyasAssetsSrc: path.join(roots.dist, names.folders.eyasAssets),
+		eyasAssetsDest: path.join(roots.eyasBuild, names.folders.eyasAssets),
+		packageJsonSrc: path.join(roots.dist, names.folders.buildAssets, names.files.packageJson),
+		packageJsonDest: path.join(roots.eyasBuild, names.files.packageJson)
 	},
 
 	config: {
-		source: path.join(consumerRoot, `.eyasrc.js`)
-	},
-
-	eyas: {}
+		source: path.join(consumerRoot, names.files.eyasrcJs)
+	}
 };
 
 // export the config for the project
