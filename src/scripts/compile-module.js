64149615818d224dbe2ea0a8c472c1c15fb958bc
Eric Higginson <eric.higginson@cycosoft.com>
2023-09-07 17:35:28 -0700

    update compilation script to automate keeping the build-assets/package.json script up-to-date

--- src/scripts/compile-module.js
+++ src/scripts/compile-module.js
@@ -2,6 +2,10 @@
 
 'use strict';
 
+// Import dependencies
+const fs = require(`fs-extra`);
+const bytenode = require(`bytenode`);
+
 // setup paths
 const path = require(`path`);
 const roots = require(`./get-roots`);
@@ -21,22 +25,23 @@ const paths = {
 	eyasAssetsSrc: path.join(roots.src, names.eyasAssets),
 	eyasAssetsDest: path.join(roots.dist, names.eyasAssets),
 	scriptsSrc: path.join(roots.src, names.scripts),
-	scriptsDest: path.join(roots.dist, names.scripts)
+	scriptsDest: path.join(roots.dist, names.scripts),
+	packageJsonModule: path.join(roots.module, `package.json`),
+	packageJsonDist: path.join(roots.dist, names.buildAssets, `package.json`)
 };
 
 // Allow for "root" await calls
 (async () => {
-	// Import dependencies
-	const fs = require(`fs-extra`);
-	const bytenode = require(`bytenode`);
-
 	// Prep the dist/ directory for module output
 	await fs.emptyDir(paths.dist);
 
 	// Copy asset directories
-	await fs.copy(paths.buildAssetsSrc, paths.buildAssetsDest);
 	await fs.copy(paths.eyasAssetsSrc, paths.eyasAssetsDest);
+	await fs.copy(paths.buildAssetsSrc, paths.buildAssetsDest);
 	await fs.copy(paths.scriptsSrc, paths.scriptsDest);
+
+	// Update the package.json version numbers
+	await updatePackageJsonValues();
 
 	// Compile the CLI
 	await fs.emptyDir(paths.cliDest);
@@ -46,3 +51,40 @@ const paths = {
 		output: paths.cliDestFile
 	});
 })();
+
+// update all the versions in the distributed package.json from the module package.json
+async function updatePackageJsonValues() {
+	// read both package.json
+	const packageJsonModule = require(paths.packageJsonModule);
+	const packageJsonDist = require(paths.packageJsonDist);
+
+	// for each key in the dist/package.json
+	for (const key in packageJsonDist) {
+		// skip if not a property of the object
+		if (!Object.hasOwnProperty.call(packageJsonDist, key)) { continue; }
+
+		// if the value is an object
+		if (typeof packageJsonDist[key] === `object`) {
+			// loop through each property
+			for (const prop in packageJsonDist[key]) {
+				// skip if not a property of the object
+				if (!Object.hasOwnProperty.call(packageJsonDist[key], prop)) { continue; }
+
+				// copy the value for each matching key from the module/package.json
+				packageJsonDist[key][prop] = packageJsonModule[key][prop];
+			}
+
+			// skip to the next key
+			continue;
+		}
+
+		// copy the value for each matching key from the module/package.json
+		packageJsonDist[key] = packageJsonModule[key];
+	}
+
+	// electron-builder requires `electron` to be a devDependency. Copy version from source.
+	packageJsonDist.devDependencies.electron = packageJsonModule.dependencies.electron;
+
+	// save the updated dist/package.json
+	await fs.outputFile(paths.packageJsonDist, JSON.stringify(packageJsonDist));
+}
