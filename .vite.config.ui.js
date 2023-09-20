d8aa5bfa688a7ecdfda5ee6cf119d00b7751e4f0
Eric Higginson <eric.higginson@cycosoft.com>
2023-09-20 16:29:59 -0700

    load vue from node module instead of cdn

--- .vite.config.ui.js
+++ .vite.config.ui.js
@@ -1,5 +1,12 @@
 import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
 
 export default {
-    root: resolve(__dirname, `./src/eyas-interface/app`)
+    plugins: [vue()],
+    root: resolve(__dirname, `./src/eyas-interface/app`),
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js'
+        }
+    }
 }
