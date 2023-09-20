2181848edf1dc4bf94b7e6cdaef617e8ace47929
Eric Higginson <eric.higginson@cycosoft.com>
2023-09-20 16:35:26 -0700

    move the vite config to its related project root

--- src/eyas-interface/app/.vite.config.ui.js
+++ src/eyas-interface/app/.vite.config.ui.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default {
     plugins: [vue()],
-    root: resolve(__dirname, `./src/eyas-interface/app`),
+    root: resolve(__dirname),
     resolve: {
         alias: {
             'vue': 'vue/dist/vue.esm-bundler.js'
