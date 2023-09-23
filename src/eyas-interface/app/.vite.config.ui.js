e3e8b49e9f82d05967257f534c03b8536bcc15dc
Eric Higginson <eric.higginson@cycosoft.com>
2023-09-23 08:06:18 -0700

    base path test change

--- src/eyas-interface/app/.vite.config.ui.js
+++ src/eyas-interface/app/.vite.config.ui.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default {
+    base: './',
     plugins: [
         vue(),
         vuetify({ autoImport: true })
