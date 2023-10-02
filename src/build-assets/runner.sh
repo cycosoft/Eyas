bc4a222907f87afe2fd1cca2a52952b8203c4806
Eric Higginson <eric.higginson@cycosoft.com>
2023-10-01 17:14:35 -0700

    run app with local install of node

--- src/build-assets/runner.sh
+++ src/build-assets/runner.sh
@@ -3,4 +3,4 @@
 # FILEPATH: /Users/erichigginson/repos/eyas/src/build-assets/runner.sh
 
 # Run local node installation from node_modules directory
-./node_modules/.bin/node "$@"
+./node_modules/.bin/node ./node_modules/.bin/electron index.js --dev
