e14f3423bd13e0bc6451cde6bc2f49a2cf9f7883
Eric Higginson <eric.higginson@cycosoft.com>
2023-10-01 17:16:01 -0700

    runner cleanup

--- src/build-assets/runner.sh
+++ src/build-assets/runner.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# FILEPATH: /Users/erichigginson/repos/eyas/src/build-assets/runner.sh
-
-# Run local node installation from node_modules directory
 ./node_modules/.bin/node ./node_modules/.bin/electron index.js --dev
