13b24e1fcfaa0d24b37cdcd086135ccf5700150f
Eric Higginson <eric.higginson@cycosoft.com>
2023-10-04 15:31:31 -0700

    saving progress

--- src/build-assets/eyas.cmd
+++ src/build-assets/eyas.cmd
@@ -1,8 +1,12 @@
 @ECHO off
 SET dp0=%~dp0
 @REM ECHO "%dp0%node_modules\.bin\node"
-ECHO "%dp0%node_modules\.bin\electron"
+@REM ECHO "%dp0%node_modules\.bin\electron"
 @REM SET NODE_PATH="%dp0%node_modules"
-set ELECTRON_ENABLE_LOGGING=true
-CALL "%dp0%node_modules\.bin\node" "%dp0%node_modules\.bin\electron" test/index.html
+SET PATH=%dp0%node_modules\.bin;%PATH%
+ECHO "NODE Path:"
+WHERE NODE
+ECHO ************
+set ELECTRON_RUN_AS_NODE=true
+CALL "%dp0%node_modules\.bin\electron" test/index.html
 PAUSE
