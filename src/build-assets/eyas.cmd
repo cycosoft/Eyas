e0c68f5ce4abf812efe22380fad85cf41d5195a1
Eric Higginson <eric.higginson@cycosoft.com>
2023-10-04 15:15:12 -0700

    windows experiments

--- src/build-assets/eyas.cmd
+++ src/build-assets/eyas.cmd
@@ -1,6 +1,8 @@
 @ECHO off
 SET dp0=%~dp0
-ECHO "%dp0%node_modules\.bin\node"
+@REM ECHO "%dp0%node_modules\.bin\node"
 ECHO "%dp0%node_modules\.bin\electron"
-CALL "%dp0%node_modules\.bin\node" "%dp0%node_modules\.bin\electron"
+@REM SET NODE_PATH="%dp0%node_modules"
+set ELECTRON_ENABLE_LOGGING=true
+CALL "%dp0%node_modules\.bin\node" "%dp0%node_modules\.bin\electron" test/index.html
 PAUSE
