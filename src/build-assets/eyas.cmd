e98904b14af2544120305f39694bec6dbf4d747a
Eric Higginson <eric.higginson@cycosoft.com>
2023-10-06 11:48:20 -0700

    updated methods of downloading the required files

--- src/build-assets/eyas.cmd
+++ src/build-assets/eyas.cmd
@@ -10,48 +10,22 @@ set nodeVersion=20.7.0
 set nodeBinSetupName=node-bin-setup
 set nodeBinSetupVersion=1.1.3
 
+set nodeWinx64Name=node-win-x64
+set nodeWinx64Version=20.7.0
+
 :: create node_modules folder if it doesn't exist
 if not exist node_modules mkdir node_modules
 if not exist node_modules\.bin mkdir node_modules\.bin
 if not exist node_modules\.downloads mkdir node_modules\.downloads
 
-:: only download if the
-
 :: only download if the packages doesn't exist
-if not exist node_modules\%npmName% goto downloadNpm
-if not exist node_modules\%nodeName% goto downloadNode
-if not exist node_modules\%nodeBinSetupName% goto downloadNodeBinSetup
-
-:downloadNpm
-:: let the user know what's downloading
-echo Downloading %npmName% %npmVersion%
-mkdir node_modules\%npmName%
-curl -L https://registry.npmjs.org/%npmName%/-/%npmName%-%npmVersion%.tgz -o node_modules\.downloads\%npmName%.tgz
-tar -xzf node_modules\.downloads\%npmName%.tgz -C node_modules\%npmName%
-move node_modules\%npmName%\package\* node_modules\%npmName%
-goto :EOF
-
-:downloadNode
-:: let the user know what's downloading
-echo Downloading %nodeName% %nodeVersion%
-mkdir node_modules\%nodeName%
-curl -L https://registry.npmjs.org/%nodeName%/-/%nodeName%-%nodeVersion%.tgz -o node_modules\.downloads\%nodeName%.tgz
-tar -xzf node_modules\.downloads\%nodeName%.tgz -C node_modules\%nodeName%
-move node_modules\%nodeName%\package\* node_modules\%nodeName%
-goto :EOF
-
-:downloadNodeBinSetup
-:: let the user know what's downloading
-echo Downloading %nodeBinSetupName% %nodeBinSetupVersion%
-mkdir node_modules\%nodeBinSetupName%
-curl -L https://registry.npmjs.org/%nodeBinSetupName%/-/%nodeBinSetupName%-%nodeBinSetupVersion%.tgz -o node_modules\.downloads\%nodeBinSetupName%.tgz
-tar -xzf node_modules\.downloads\%nodeBinSetupName%.tgz -C node_modules\%nodeBinSetupName%
-move node_modules\%nodeBinSetupName%\package\* node_modules\%nodeBinSetupName%
-goto :EOF
+if not exist node_modules\%npmName% CALL installModule.cmd %npmName% %npmVersion%
+@REM if not exist node_modules\%nodeName% CALL installModule.cmd %nodeName% %nodeVersion%
+@REM if not exist node_modules\%nodeBinSetupName% CALL installModule.cmd %nodeBinSetupName% %nodeBinSetupVersion%
+if not exist node_modules\%nodeWinx64Name% CALL installModule.cmd %nodeWinx64Name% %nodeWinx64Version%
 
 :: remove the downloads directory
 rmdir /s /q node_modules\.downloads
-
 
 :: Set the working directory to the directory of the script
 @REM cd /d "%~dp0"
