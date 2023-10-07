257e49ee3f2e16de7814aa5d2fe1b5a2aa49ef65
Eric Higginson <eric.higginson@cycosoft.com>
2023-10-07 08:19:43 -0700

    log the portable artifact path

--- src/cli/index.js
+++ src/cli/index.js
@@ -397,6 +397,7 @@ async function runCommand_compile() {
 		const archive = archiver(`zip`, { zlib: 9 });
 		output.on(`close`, () => {
 			userLog();
+			userLog(`File created -> ${path.join(paths.dist, `${artifactName}.zip`)}`);
 			userLog(`Portable compilation complete!`);
 			userLog();
 		});
