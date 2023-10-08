42d25dc47de8cf6b696925fd718bad0322807321
Eric Higginson <eric.higginson@cycosoft.com>
2023-10-08 07:48:52 -0700

    updates to eyas.cmd to make it work across all states

--- src/build-assets/eyas.cmd
+++ src/build-assets/eyas.cmd
@@ -3,17 +3,19 @@
 :: module definitions
 set npmName=npm
 set npmVersion=10.2.0
-set nodeWinx64Name=node-win-x64
-set nodeWinx64Version=20.7.0
+set nodeName=node-win-x64
+set nodeVersion=20.7.0
 
 :: create node_modules folder if it doesn't exist
 if not exist node_modules mkdir node_modules
 if not exist node_modules\.bin mkdir node_modules\.bin
 if not exist node_modules\.downloads mkdir node_modules\.downloads
 
-:: only download if the packages don't exist
+:: download NPM if it doesn't exist yet
 if not exist node_modules\%npmName% CALL installModule.cmd %npmName% %npmVersion%
-if not exist node_modules\%nodeWinx64Name% CALL installModule.cmd %nodeWinx64Name% %nodeWinx64Version%
+
+:: download Node if the initial runner doesn't exist AND if `npm i` hasn't run
+if not exist node_modules\%nodeName% if not exist node_modules\node CALL installModule.cmd %nodeName% %nodeVersion%
 
 :: remove the downloads directory
 rmdir /s /q node_modules\.downloads
@@ -24,5 +26,8 @@ cd /d "%~dp0"
 :: temporarily set the PATH to the local installation of node
 set PATH=%cd%\node_modules\.bin;%PATH%
 
-:: use node to run npm
-CALL node node_modules\npm\bin\npm-cli.js %* start
+:: Install npm and node properly if `npm i` hasn't been run yet
+if not exist node_modules\node CALL node node_modules\npm\bin\npm-cli.js i npm@%npmVersion% node@%nodeVersion%
+
+:: Run Eyas
+CALL npm start
