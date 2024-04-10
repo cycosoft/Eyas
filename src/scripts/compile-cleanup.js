d41f39e03d6687b1bfb02ae26e12e24c165cc444
Eric Higginson <eric.higginson@cycosoft.com>
2024-04-09 17:34:09 -0700

    remove the .pre-build folder at process end

--- src/scripts/compile-cleanup.js
+++ src/scripts/compile-cleanup.js
@@ -14,8 +14,10 @@ const path = require(`path`);
 	const consumerRoot = process.cwd();
 	const buildRoot = path.join(consumerRoot, `.build`);
 	const runnersRoot = path.join(consumerRoot, `.runners`);
+	const preBuildRoot = path.join(consumerRoot, `.pre-build`);
 
 	// Remove the .build/ & .runners/ directories
 	await fs.remove(buildRoot);
 	await fs.remove(runnersRoot);
+	await fs.remove(preBuildRoot);
 })();
