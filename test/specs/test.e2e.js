1d2c3e9b60c7fc0ea81c992bd0b193b8f3d478ea
Eric Higginson <eric.higginson@cycosoft.com>
2024-07-11 21:36:52 -0700

    test linting

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,8 +1,8 @@
 import { browser } from '@wdio/globals'
 
-describe('Electron Testing', () => {
-    it('should print application title', async () => {
-        console.log('Hello', await browser.getTitle(), 'application!')
-    })
-})
+describe(`Electron Testing`, () => {
+	it(`should print application title`, async () => {
+		console.log(`Hello`, await browser.getTitle(), `application!`)
+	});
+});
 
