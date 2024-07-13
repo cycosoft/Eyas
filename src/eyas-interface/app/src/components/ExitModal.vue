2bf439dc17a749582df25bcc4a128decf0d12956
Eric Higginson <eric.higginson@cycosoft.com>
2024-07-13 06:49:29 -0700

    remove the overlay from the exit modal and simplify

--- src/eyas-interface/app/src/components/ExitModal.vue
+++ src/eyas-interface/app/src/components/ExitModal.vue
@@ -1,50 +1,35 @@
 <template>
-	<v-overlay
+	<v-dialog
 		v-model="visible"
-		class="exit-modal text-white"
+		persistent
+		width="auto"
+		data-qa="exit-modal"
+		:scrim="false"
+		@after-leave="hideUi"
 	>
-		<div class="ad-space">
-			<span class="cursor-pointer" @click="openInBrowser(`https://cycosoft.com`)">
-				<img
-					alt="Cycosoft, LLC logo"
-					src="@/assets/cycosoft-logo.svg"
-					width="175"
+		<v-card>
+			<v-card-text>
+				Would you like to exit the test?
+			</v-card-text>
+
+			<v-card-actions class="mt-5">
+				<v-btn @click="cancel">
+					Cancel
+				</v-btn>
+
+				<div class="flex-grow-1" />
+
+				<v-btn
+					color="error"
+					variant="elevated"
+					:loading="exiting"
+					@click="exit"
 				>
-			</span>
-		</div>
-
-		<v-dialog
-			v-model="visible"
-			persistent
-			width="auto"
-			data-qa="exit-modal"
-			:scrim="false"
-			@after-leave="hideUi"
-		>
-			<v-card>
-				<v-card-text>
-					Would you like to exit the test?
-				</v-card-text>
-
-				<v-card-actions class="mt-5">
-					<v-btn @click="cancel">
-						Cancel
-					</v-btn>
-
-					<div class="flex-grow-1" />
-
-					<v-btn
-						color="error"
-						variant="elevated"
-						:loading="exiting"
-						@click="exit"
-					>
-						Exit
-					</v-btn>
-				</v-card-actions>
-			</v-card>
-		</v-dialog>
-	</v-overlay>
+					Exit
+				</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
 </template>
 
 <script>
@@ -76,25 +61,7 @@ export default {
 			if(document.querySelectorAll(`.v-dialog`).length <= 1) {
 				window.eventBridge?.send(`hide-ui`);
 			}
-		},
-
-		openInBrowser(url) {
-			window.eventBridge?.send(`open-in-browser`, url);
 		}
 	}
 };
 </script>
-
-<style scoped lang="scss">
-.exit-modal:deep(.v-overlay__content){
-	width: 100%;
-	height: 100%;
-
-	.ad-space {
-		display: flex;
-		height: 100%;
-		align-items: flex-end;
-		justify-content: flex-end;
-	}
-}
-</style>
