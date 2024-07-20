409983f201636654fc4f11954bed8fbe0e7d760f
Eric Higginson <eric.higginson@cycosoft.com>
2024-07-20 06:28:48 -0700

    linting

--- src/eyas-interface/app/src/components/ExitModal.vue
+++ src/eyas-interface/app/src/components/ExitModal.vue
@@ -41,9 +41,7 @@ export default {
 
 	mounted() {
 		// Listen for messages from the main process
-		window.eventBridge?.receive(`modal-exit-visible`, value => {
-			this.visible = value;
-		});
+		window.eventBridge?.receive(`modal-exit-visible`, value => this.visible = value);
 	},
 
 	methods: {
