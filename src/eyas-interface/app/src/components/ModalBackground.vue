192498a3a1b9bd7725b0dbb98a70e300844bb949
Eric Higginson <eric.higginson@cycosoft.com>
2024-07-22 07:46:59 -0700

    create a custom component to manage the background overlay for modals

---
+++ src/eyas-interface/app/src/components/ModalBackground.vue
@@ -0,0 +1,43 @@
+<template>
+	<v-overlay class="custom-background" :model-value="modelValue">
+		<div class="bottom-right">
+			<span class="cursor-pointer" @click="openInBrowser(`https://cycosoft.com`)">
+				<img
+					alt="Cycosoft, LLC logo"
+					src="@/assets/cycosoft-logo.svg"
+					width="175"
+				>
+			</span>
+		</div>
+	</v-overlay>
+
+	<slot />
+</template>
+
+<script>
+export default {
+	props: {
+		modelValue: Boolean
+	},
+
+	methods: {
+		openInBrowser(url) {
+			window.eventBridge?.send(`launch-link`, { url, openInBrowser: true });
+		}
+	}
+};
+</script>
+
+<style scoped lang="scss">
+.custom-background:deep(.v-overlay__content){
+	width: 100%;
+	height: 100%;
+
+	.bottom-right {
+		display: flex;
+		height: 100%;
+		align-items: flex-end;
+		justify-content: flex-end;
+	}
+}
+</style>
