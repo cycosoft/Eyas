1c617a6aa94145094baa4b0fd389369343fee07a
Eric Higginson <eric.higginson@cycosoft.com>
2024-07-22 07:47:34 -0700

    use the new modal background on the exit modal

--- src/eyas-interface/app/src/components/ExitModal.vue
+++ src/eyas-interface/app/src/components/ExitModal.vue
@@ -1,46 +1,53 @@
 <template>
-	<v-dialog
-		v-model="visible"
-		width="auto"
-		persistent
-		data-qa="exit-modal"
-		:scrim="false"
-		@after-leave="hideUi"
-		@keyup.esc="cancel"
-		@keyup.enter="exit"
-		@keyup.e="exit"
-	>
-		<v-card>
-			<v-card-text>
-				Would you like to exit the test?
-			</v-card-text>
+	<ModalBackground :model-value="visible">
+		<v-dialog
+			v-model="visible"
+			width="auto"
+			persistent
+			data-qa="exit-modal"
+			:scrim="false"
+			@after-leave="hideUi"
+			@keyup.esc="cancel"
+			@keyup.enter="exit"
+			@keyup.e="exit"
+		>
+			<v-card>
+				<v-card-text>
+					Would you like to exit the test?
+				</v-card-text>
 
-			<v-card-actions class="mt-5">
-				<v-btn @click="cancel">
-					Cancel
-				</v-btn>
+				<v-card-actions class="mt-5">
+					<v-btn @click="cancel">
+						Cancel
+					</v-btn>
 
-				<div class="flex-grow-1" />
+					<div class="flex-grow-1" />
 
-				<v-btn
-					color="error"
-					variant="elevated"
-					:loading="exiting"
-					@click="exit"
-				>
-					<u>E</u>xit
-				</v-btn>
-			</v-card-actions>
-		</v-card>
-	</v-dialog>
+					<v-btn
+						color="error"
+						variant="elevated"
+						:loading="exiting"
+						@click="exit"
+					>
+						<u>E</u>xit
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
+	</ModalBackground>
 </template>
 
 <script>
+import ModalBackground from '@/components/ModalBackground.vue';
 export default {
 	data: () => ({
 		visible: false,
 		exiting: false
 	}),
+
+	components: {
+		ModalBackground
+	},
 
 	mounted() {
 		// Listen for messages from the main process
